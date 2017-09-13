4571e719cf7c7fcab840d0524e5189fe8b7c4e7f
cmactaggart <mtags22@gmail.com>
2017-09-12 19:20:43 -0600

    passing params to specific functions

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log('I wonder if they will go back and look at the very first check-in' + process.argv + process.argv.length);
+var fs = require('fs');
+
+let args = process.argv.slice(2);
+
+if(args.length > 0){
+    let commandName = args[0];
+    let params = args.slice(1);
+    switch(commandName){
+        case 'add':
+            addValueToStore(...params);
+            break;
+        case 'list':
+            listValuesInStore();
+            break;
+        case 'get':
+            getValueFromStore(...params);
+            break;
+        case 'remove':
+            removeValueFromStore(...params);
+            break;
+    }
+} else{
+    //todo:  spit out some help.
+    console.log("No commands provided.");
+}
+
+function addValueToStore(key, value){
+    console.log('adding' + key + value);
+
+    console.log(__dirname);
+}
+
+function listValuesInStore(){
+    console.log('listing');
+}
+
+function getValueFromStore(){
+    console.log('getting');
+}
+
+function removeValueFromStore(){
+    console.log('removing');
+}
+
+
+// fs.writeFile("/tmp/test", "Hey there!", function(err) {
+//     if(err) {
+//         return console.log(err);
+//     }
+
+//     console.log("The file was saved!");
+// }); 
