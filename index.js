9f45719a893c228385bc17d66c868041a7272bf0
cmactaggart <mtags22@gmail.com>
2017-09-12 22:27:03 -0600

    completed bug fixes

--- index.js
+++ index.js
@@ -1,124 +1,135 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-var fs = require('fs');
+var fs = require("fs");
 
 const keyValueFilePath = __dirname + "\\_store.json";
 
 let args = process.argv.slice(2);
 
 //determine command
-if(args.length > 0){
-    let commandName = args[0];
-    let params = args.slice(1);
-    switch(commandName){
-        case 'add':
-            addValueToStore(...params);
-            break;
-        case 'list':
-            listValuesInStore();
-            break;
-        case 'get':
-            getValueFromStore(...params);
-            break;
-        case 'remove':
-            removeValueFromStore(...params);
-            break;
-    }
+if (args.length > 0) {
+  let commandName = args[0];
+  let params = args.slice(1);
+  switch (commandName) {
+    case "add":
+      addValueToStore(...params);
+      break;
+    case "list":
+      listValuesInStore();
+      break;
+    case "get":
+      getValueFromStore(...params);
+      break;
+    case "remove":
+      removeValueFromStore(...params);
+      break;
+    default:
+      console.log("unknown command");
+  }
 } else {
-    //todo:  spit out some help.
-    console.log("No commands provided.");
+  console.log(
+    "No valid command provided.  Refer to README.md for valid commands and usage."
+  );
 }
 
 //loads the keyvalue object from file and into an object
-function loadKeyValueObject(success, error){
-    fs.readFile(keyValueFilePath, function (err, data) {  
-        let keyValueObj = undefined;
-        if (err != undefined && err.code !== "ENOENT") {  
-            //error other than file/directory doesnt exist
-            error(err);
-        } else {
-            keyValueObj = new Object();
-            if(data != undefined){   
-                //todo:  check correupt file  
-                keyValueObj = JSON.parse(data);                
-            }
-            success(keyValueObj);
-        }        
-    });
-}
-
-function saveKeyValueObject(keyValueObj, success, error){
-    fs.writeFile(keyValueFilePath, JSON.stringify(keyValueObj), function(err){
-        if(err) {
-            error(err);
+function loadKeyValueObject(success, error) {
+  fs.readFile(keyValueFilePath, function(err, data) {
+    let keyValueObj = undefined;
+    if (err != undefined && err.code !== "ENOENT") {
+      //error other than file/directory doesnt exist
+      error(err);
+    } else {
+      keyValueObj = new Object();
+      if (data != undefined) {
+        try {
+          keyValueObj = JSON.parse(data);
+        } catch (e) {
+          console.log("storage file has become corrupt - " + keyValueFilePath);
         }
-        success();
-    });
+      }
+      success(keyValueObj);
+    }
+  });
 }
 
-function addValueToStore(key, value){
-    loadKeyValueObject(function(keyValueObj){
-            keyValueObj[key] = value;
-
-            saveKeyValueObject(keyValueObj, function(){
-                //todo:  better message
-                console.log("The file was saved!");
-                process.exit();
-            },
-            (err)=> console.log(err)
-        );
-
-    }, 
-        //todo:  better error message
-        (err)=>{console.log("error loading data")}
-    );
-    
+//saves the keyvalue object to file
+function saveKeyValueObject(keyValueObj, success, error) {
+  fs.writeFile(keyValueFilePath, JSON.stringify(keyValueObj), function(err) {
+    if (err) {
+      error(err);
+    }
+    success();
+  });
 }
 
-function removeValueFromStore(key){
-    loadKeyValueObject(function(keyValueObj){
-        delete keyValueObj[key];
-        saveKeyValueObject(keyValueObj, function(){
-            //todo:  better message
-            console.log("The file was saved!");
-            process.exit();
+//loads keyvalue object, add the specified keyvalue, then saves the object to file
+function addValueToStore(key, value) {
+  if (key == undefined || value == undefined) {
+    invalidParametersHandler();
+  } else {
+    loadKeyValueObject(function(keyValueObj) {
+      keyValueObj[key] = value;
+
+      saveKeyValueObject(
+        keyValueObj,
+        function() {
+          console.log("changes were saved");
         },
-        (err)=> console.log(err)
-    );
-
-    }, 
-        //todo:  better error message
-    (err)=>{console.log("error loading data")}
-);
+        err => console.log(err)
+      );
+    }, errorLoadingDataHandler);
+  }
 }
 
-
-
-
-function listValuesInStore(){
-    loadKeyValueObject(function(keyValueObj){
-        for(var key in keyValueObj){
-            if(keyValueObj.hasOwnProperty(key)){
-                console.log(key + '\t' + keyValueObj[key]);
-            }
-        }
-        process.exit();
-        
-    },   
-    //todo:  better error message
-    (err)=>{console.log("error loading data")}
-);
+//loads keyvalue object, removes the specified keyvalue, then saves the object to file
+function removeValueFromStore(key) {
+  if (key == undefined) {
+    invalidParametersHandler();
+  } else {
+    loadKeyValueObject(function(keyValueObj) {
+      delete keyValueObj[key];
+      saveKeyValueObject(
+        keyValueObj,
+        function() {
+          console.log("changes were saved");
+        },
+        err => console.log(err)
+      );
+    }, errorLoadingDataHandler);
+  }
 }
 
-function getValueFromStore(key){
-    loadKeyValueObject(function(keyValueObj){
-        console.log(keyValueObj[key]);
-        process.exit();
-        
-    },   
-    //todo:  better error message
-    (err)=>{console.log("error loading data")}
-);
+//loads keyvalue object, outputs the keyvalues to the console log
+function listValuesInStore() {
+  loadKeyValueObject(function(keyValueObj) {
+    let counter = 0;
+    for (var key in keyValueObj) {
+      if (keyValueObj.hasOwnProperty(key)) {
+        console.log(key + "\t" + keyValueObj[key]);
+        counter++;
+      }
+    }
+    console.log("# of keys: " + counter);
+  }, errorLoadingDataHandler);
 }
 
+//loads keyvalue object, outputs the specified value, based on the key, to the console log
+function getValueFromStore(key) {
+  if (key == undefined) {
+    invalidParametersHandler();
+  } else {
+    loadKeyValueObject(function(keyValueObj) {
+      console.log(keyValueObj[key]);
+    }, errorLoadingDataHandler);
+  }
+}
+
+function errorLoadingDataHandler(err) {
+  console.log("error loading data - " + err);
+}
+
+function invalidParametersHandler() {
+  console.log("invalid parameters");
+}
